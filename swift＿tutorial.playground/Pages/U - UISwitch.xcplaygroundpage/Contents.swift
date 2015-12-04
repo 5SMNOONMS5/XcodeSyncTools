5a450b2b5317132b98e59cd28b90ab74364c2b74
rocooshiang <rocoo.shaing@gmail.com>
2015-12-05 01:14:16 +0800

    Add UISwitch

---
+++ swift＿tutorial.playground/Pages/U - UISwitch.xcplaygroundpage/Contents.swift
@@ -0,0 +1,28 @@
+
+import Foundation
+import UIKit
+
+class viewcontroller: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //UISwitch寬高已經是固定的，不需要在設定
+        let mySwitch = UISwitch(frame: CGRect(x: 100, y: 100, width: 0, height: 0))
+        mySwitch.setOn(true, animated: true)
+        //監聽UISwitch開關變動通知
+        mySwitch.addTarget(self, action: "switchIsChanged:", forControlEvents: UIControlEvents.ValueChanged)
+        self.view.addSubview(mySwitch)
+        
+    }
+    
+    func switchIsChanged(sender: UISwitch){
+        print("Sender is \(sender)")
+        
+        if sender.on{
+            print("The switch is turned on")
+        }else{
+            print("The switch is turned off")
+        }
+    }
+    
+}
+
