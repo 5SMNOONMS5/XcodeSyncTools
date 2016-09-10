9b58f0dddf89a9b70e5594e6d48ba71c510eac4e
stephen <root>
2016-09-10 15:33:39 +0800

    Add fetch.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,16 @@
 
 
-cp swift/*.codesnippets ~/Library/Developer/Xcode/UserData/CodeSnippets
 
-alias cdCodeSnippet='cd ~/Library/Developer/Xcode/UserData/CodeSnippets'
+$DIRECTORY="~/Library/Developer/Xcode/UserData/CodeSnippets"
 
-open -a finder ./
+if [ -d "$DIRECTORY" ]; then
+
+  cp swift/*.codesnippets ~/Library/Developer/Xcode/UserData/CodeSnippets
+
+  alias cdCodeSnippet='cd ~/Library/Developer/Xcode/UserData/CodeSnippets'
+
+  open -a finder ./
+
+else
+  echo "~/Library/Developer/Xcode/UserData/CodeSnippets doesn't exist , please create an one "
+fi
