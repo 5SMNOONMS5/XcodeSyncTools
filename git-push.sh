b5c2203981a5b7f9b8ef4c95ab2fea054777cfd9
stephen <root>
2016-09-13 18:44:07 +0800

    Update git-push.sh

--- git-push.sh
+++ git-push.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
-
 source ~/.bash_profile
 
 #fetch all arguments via $@
 message=$@
 
-# run when files change
 if [[ $(git diff) ]]; then
 
   git add -A
 
-  git commit -m "$message"
+  git commit -m '$message'
 
   git push
 
