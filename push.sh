abca9910f454b9ea2d19261be82e0a428a98848b
stephen <root>
2016-09-14 14:42:34 +0800

    update

--- push.sh
+++ push.sh
@@ -4,19 +4,29 @@ source ~/.bash_profile
 #fetch all arguments via $@
 message=$@
 
+echo $message
+
 if [[ $(git diff) ]]; then
 
-  find ~/Library/Developer/Xcode/UserData/CodeSnippets/*.codesnippet -type f -exec cp {} ./xcode \;
+  # check message is empty
+  if [[ -z "$message"  ]]; then
 
-  git add -A
+    echo "no commit message . abort !"
 
-  git commit -m $message
+  else
 
-  git push
+    find ~/Library/Developer/Xcode/UserData/CodeSnippets/*.codesnippet -type f -exec cp {} ./xcode \;
+
+    git add -A
+
+    git commit -m $message
+
+    git push
+
+  fi
 
 else
 
   echo "not thing to commit , working directory clear"
 
-  exit
 fi
