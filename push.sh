095c26fbb1de2ee33f795aa14cd75c11eb0938e4
5SMNOONMS5 <tasb00429@gmail.com>
2016-09-25 23:48:35 +0800

    aaa

--- push.sh
+++ push.sh
@@ -4,24 +4,31 @@ source ~/.bash_profile
 #fetch all arguments via $@
 message=$@
 
+PATHXCODE=~/Library/Developer/Xcode/UserData/CodeSnippets
+
 if [[ $(git diff) ]]; then
 
   # find ~/Library/Developer/Xcode/UserData/CodeSnippets/*.codesnippet -type f -exec cp {} ./xcode \;
 
-  # check message is empty
+  # check commit message
   if [[ -z "${message}"  ]]; then
 
     echo "no commit message . abort !"
 
   else
 
-    # find ~/Library/Developer/Xcode/UserData/CodeSnippets/*.codesnippet -type f -exec cp {} ./xcode \;
+    rm ./xcode/*
 
+    echo "update old codesnippet under xcode folder to newest one"
+    cp ${PATHXCODE}/* ./xcode
+
+    echo "start push"
     git add -A
 
     git commit -m "${message}"
 
     git push
+    echo "done"
 
   fi
 
