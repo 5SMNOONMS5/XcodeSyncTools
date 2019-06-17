cb9b9dd0d2232fc1559a290e69324ccfd08f3c9f
stephen <tasb00429@gmail.com>
2019-06-17 09:44:13 +0800

    [ Update ]：

---
+++ File Templates/MVVM/Client.xctemplate/___FILEBASENAME___Model.swift
@@ -0,0 +1,15 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
+//
+
+import Aletheia
+import Alamofire
+
+/// ___FILEBASENAMEASIDENTIFIER___
+struct ___FILEBASENAMEASIDENTIFIER___: Decodable {
+
+}
